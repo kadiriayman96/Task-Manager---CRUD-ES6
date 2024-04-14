dc84bfde255b679b156f39c2cd20a775bf2a6377
kadiriayman96 <ayman.kadev@gmail.com>
2024-04-14 13:09:45 +0100

    Fonction SUpprimer Tache

--- script.js
+++ script.js
@@ -12,14 +12,34 @@ class TODO {
     this.AfficherTaches();
   }
 
+  supprimerTache(idTache) {
+    this.taches = this.taches.filter((tache) => tache.id != idTache);
+    this.AfficherTaches();
+  }
+
   AfficherTaches() {
     var liste = document.getElementById("taskList");
 
     liste.innerHTML = "";
     this.taches.forEach((tache) => {
       var li = document.createElement("li");
-      li.innerHTML = `<span>${tache.tache}</span>  `;
+      li.innerHTML = `
+                <span>${tache.tache}</span>
+                <button class="delete-btn" data-id="${tache.id}">Delete</button>
+      `;
       liste.appendChild(li);
+    });
+
+    this.SupprimerTacheEvent();
+  }
+
+  SupprimerTacheEvent() {
+    var deleteBtns = document.querySelectorAll(".delete-btn");
+    deleteBtns.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        let idTache = btn.getAttribute("data-id");
+        this.supprimerTache(idTache);
+      });
     });
   }
 }
