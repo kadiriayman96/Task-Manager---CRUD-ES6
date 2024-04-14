ad922d1203ce70e2cb4233a7199e6de93066be61
kadiriayman96 <ayman.kadev@gmail.com>
2024-04-14 12:55:40 +0100

    Function Afficher les taches

--- script.js
+++ script.js
@@ -9,5 +9,29 @@ class TODO {
       tache: nomTache,
     };
     this.taches.push(tache);
+    this.AfficherTaches();
+  }
+
+  AfficherTaches() {
+    var liste = document.getElementById("taskList");
+
+    liste.innerHTML = "";
+    this.taches.forEach((tache) => {
+      var li = document.createElement("li");
+      li.innerHTML = `<span>${tache.tache}</span>  `;
+      liste.appendChild(li);
+    });
   }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let taches = new TODO();
+
+  document.getElementById("taskForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    var nomTache = document.getElementById("taskInput");
+    taches.ajouterTache(nomTache.value);
+    nomTache.value = "";
+  });
+});
